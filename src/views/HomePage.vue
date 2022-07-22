d2ebbd275c3801439ae2feab9e7436a203210937
Arham Roshan <arhamroshan@Arhams-MacBook-Pro.local>
2022-07-22 21:56:16 +0500

    rename inbox

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Inbox</ion-title>
+        <ion-title>XSense SDK</ion-title>
       </ion-toolbar>
     </ion-header>
 
